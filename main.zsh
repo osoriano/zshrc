45faafd5f007ff406fa1336c62bd483cd2d0dd43
Omar Soriano <osoriano2@gmail.com>
2024-09-21 12:09:13 -0700

    Add `loadnvm` command
    Instead of using nvm in each shell session (which has a startup cost),
    add a function to load it when needed

--- main.zsh
+++ main.zsh
@@ -158,5 +158,8 @@ export PATH="/opt/go/bin:${PATH}"
 # From nvm git install instructions
 # See https://github.com/nvm-sh/nvm#git-install
 export NVM_DIR="$HOME/gh/nvm-sh/nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+loadnvm() {
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+  [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+}
